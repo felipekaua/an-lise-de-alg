2b654110c279f8959254ffa8f448c5523109a3e6
felipekaua <fkauadelima@gmail.com>
2022-06-01 14:55:58 -0300

    Exercício 2.11 feito

---
+++ Exercício 2.11/Código.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void acharEntrada(int entrada, int i, int j, int z, int y){
+    if(i>=0){     
+            if(entrada!=z){
+                z++;
+                acharEntrada(entrada, i-1, j, z, y);
+            }else{
+                j=j-i;
+                printf("linha: %d", i);
+                printf("\ncoluna: %d", j);
+            }
+    }else{
+            y++;
+            i=i+y;
+            j=i;
+            acharEntrada(entrada, i, j, z, y);
+    }
+}
+
+int main(){
+    int i=-1, j=0, y=0, z=0;
+    int entrada, linha=0, coluna=0;
+    printf("Insira um numero: ");
+    scanf("%d", &entrada);
+    acharEntrada(entrada, i, j, z, y);
+    
+    return 0;
+}
